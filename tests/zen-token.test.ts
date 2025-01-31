ad821794583a95b381defc1121c06f27af161910
Happiness0653 <happylove6904@gmail.com>
2025-01-31 21:24:16 +0100

    test: Add zen token tests

---
+++ tests/zen-token.test.ts
@@ -0,0 +1,15 @@
+import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarinet@v1.0.0/index.ts';
+import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
+
+Clarinet.test({
+  name: "Ensure that token minting works",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    const deployer = accounts.get('deployer')!;
+    const wallet1 = accounts.get('wallet_1')!;
+    
+    let block = chain.mineBlock([
+      Tx.contractCall('zen-token', 'mint', [types.uint(50), types.principal(wallet1.address)], deployer.address)
+    ]);
+    assertEquals(block.receipts[0].result, '(ok true)');
+  },
+});
